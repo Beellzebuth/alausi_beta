a54fe1fedb12db77c706063cee8153c6647ea8fa
Beellzebuth <josepevil08@gmail.com>
2024-04-13 22:47:17 -0500

    cambio de prueba 1

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Programa de Prueba #1',
+      title: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
       theme: ThemeData(
         primaryColor: Colors.blue,
       ),
